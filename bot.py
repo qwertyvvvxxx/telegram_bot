3f41c0073a4fb4d5ee1c19be6451883ed31f3837
qwertyvvvxxx <ivannavidiatchuk@gmail.com>
2026-03-09 14:21:10 +0200

    add: gpt func

--- bot.py
+++ bot.py
@@ -1,10 +1,23 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler, ConversationHandler, MessageHandler, filters
 
 from credentials import config
 from gpt import ChatGptService
 from util import (load_message, send_text, send_image, show_main_menu,
                   default_callback_handler, load_prompt)
+from telegram import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+GPT: int = 1
+
+# клава (кнопка) для чату щоб виключити режим GPT
+
+gpt_keyboard = ReplyKeyboardMarkup([[KeyboardButton("Закінчити")]], resize_keyboard=True)
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -23,19 +36,57 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     })
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
     prompt_text = load_prompt('random')
-    message = await update.message.reply_text("Обробка команди, зачекайте...")
+    message = await update.message.reply_text(load_message('random'))
     chat_gpt.set_prompt(prompt_text)
     response_text = await chat_gpt.send_message_list()
     await message.edit_text(response_text)
 
-chat_gpt = ChatGptService(config.GPT_API_KEY)
-app = ApplicationBuilder().token(config.BOT_TOKEN).build()
+async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_gpt.set_prompt(load_prompt('gpt'))
+    await send_image(update, context, 'gpt')
+    await update.message.reply_text(load_message('gpt'), reply_markup=gpt_keyboard)
 
-# Зареєструвати обробник команди можна так:
-app.add_handler(CommandHandler('start', start))
-app.add_handler(CommandHandler('random', random))
-# Зареєструвати обробник колбеку можна так:
-# app.add_handler(CallbackQueryHandler(app_button, pattern='^app_.*'))
-app.add_handler(CallbackQueryHandler(default_callback_handler))
-print("помилки немає")
-app.run_polling()
+    return GPT
+
+async def gpt_dialog(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+
+    if user_text == "Закінчити":
+        await update.message.reply_text("Режим ChatGPT вимкненно", reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+
+    message = await update.message.reply_text("ChatGPT готує відповідь...")
+    response_text = await chat_gpt.send_question(user_text)
+
+    await message.edit_text(response_text)
+
+    return GPT
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+chat_gpt = ChatGptService(config.GPT_API_KEY)
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(config.BOT_TOKEN).build()
+
+    gpt_handler = ConversationHandler(
+        entry_points=[CommandHandler('gpt', gpt)],
+        states={
+            # Коли ми в стані GPT (1), чекаємо на текст від користувача
+            GPT: [MessageHandler(filters.TEXT & ~filters.COMMAND, gpt_dialog)]
+        },
+        fallbacks=[CommandHandler('start', start)] # Вихід з режиму при /start
+    )
+
+    # РЕЄСТРУЮ ХЕНДЛЕРИ:
+    app.add_handler(gpt_handler)
+    app.add_handler(CommandHandler('random', random)) # Потім прості команди
+    app.add_handler(CommandHandler('start', start)) # Потім головне меню
+    print("-----------------бота запущено без помилок-----------------")
+    app.run_polling()
